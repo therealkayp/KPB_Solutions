39b200469757e4286b687f475d6d9fa1929f93d0
Kadia Prince <kadiaaprince@gmail.com>
2024-10-03 18:59:35 -0400

    Initial commit with all website files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"; // Enable strict mode
+
+window.addEventListener('scroll', function () {
+  let navbar = document.getElementById('navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
